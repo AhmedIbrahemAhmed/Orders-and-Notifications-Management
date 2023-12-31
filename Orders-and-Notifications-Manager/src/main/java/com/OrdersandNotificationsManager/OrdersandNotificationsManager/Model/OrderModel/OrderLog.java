76f136cafc3e24eaeac15ae5aec73b69fa180ccf
AhmedIbrahemAhmed <96237323+AhmedIbrahemAhmed@users.noreply.github.com>
2023-12-31 09:34:47 +0200

    made the orderlog class print after successful addition and deletion for testing

--- Orders-and-Notifications-Manager/src/main/java/com/OrdersandNotificationsManager/OrdersandNotificationsManager/Model/OrderModel/OrderLog.java
+++ Orders-and-Notifications-Manager/src/main/java/com/OrdersandNotificationsManager/OrdersandNotificationsManager/Model/OrderModel/OrderLog.java
@@ -7,9 +7,8 @@ import java.util.Objects;
 import java.util.Vector;
 @Component
 public class OrderLog {
-    private final Vector<Order> orders ;
+    private final Vector<Order> orders = new Vector<>();
     public OrderLog(){
-        this.orders = new Vector<>() ;
     }
     public void create(Order order) throws Exception {
         for(Order order1: orders){
@@ -17,11 +16,15 @@ public class OrderLog {
                 throw new Exception("order with same ID already exists") ;
         }
         orders.add(order) ;
+        System.out.print("created order succesfully with id: ");
+        System.out.print(order.getID());
+        System.out.println();
     }
     public void delete(String ID){
         for(int i=0;i<orders.size();i++){
             if(Objects.equals(orders.elementAt(i).getID(), ID)) {
                 orders.remove(i);
+                System.out.println("deletion success");
                 break;
             }
         }
