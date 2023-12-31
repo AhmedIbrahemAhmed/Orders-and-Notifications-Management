6526c634ac3cbb1dcd7a04d135b2c32a29fd0cfa
AhmedIbrahemAhmed <96237323+AhmedIbrahemAhmed@users.noreply.github.com>
2023-12-31 13:21:17 +0200

    Delete Orders-and-Notifications-Manager/src/main/java/com/OrdersandNotificationsManager/OrdersandNotificationsManager/Order directory
    replaced with more structured packages

--- Orders-and-Notifications-Manager/src/main/java/com/OrdersandNotificationsManager/OrdersandNotificationsManager/Order/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.OrdersandNotificationsManager.OrdersandNotificationsManager.Order;
-
-public abstract class OrderService {
-    public abstract void ship(String ID) ;
-
-    public abstract void cancel(String ID) ;
-
-    public abstract void cancelShipment(String ID) ;
-}
